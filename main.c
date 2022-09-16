dbc8874ba97c74a600c63aca8d6efc65edd9801b
Peternyamari <113664817+Peternyamari@users.noreply.github.com>
2022-09-16 20:18:28 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello World,\n");
+    printf("welcome to my first project\n");
+    printf("my name is Peter nyamari\n");
+    printf("from Zetech University.\n");
+    return 0;
+}
